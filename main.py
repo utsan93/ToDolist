50e286194853937fa695ffb180954af8a323bf6a
Mark Glushakov <glushakov.m.e@gmail.com>
2025-07-05 13:20:27 +0300

    123

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-def work():
-    print("go and fuck yourself")
+import sqlite3
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index1():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
